b68a5e03259765bff0bc80d45a0878588bfe15ba
Arno-Dubois <144135021+Arno-Dubois@users.noreply.github.com>
2023-09-07 14:57:20 +0200

    Menu

--- main.c
+++ main.c
@@ -7,14 +7,42 @@ int assignTable(int guestCount) {
     else return 0;
 }
 
+int menuPrice(char dish,int drink,int dessert) {
+    int total = 0;
+    
+    switch (dish) {
+        case 'c': total += 15;
+        case 't': total += 8;
+        case 'r': total += 23;
+        default: return 0;
+    }
+
+    switch (drink) {
+        case 's': total += 5;
+        case 'e': total += 2;
+        case 'l': total += 12;
+        default: return 0;
+    }
+    printf("%d", total);
+}
+
 int main() {
     int guestCount;
+    char dish, drink, dessert;
 
     printf("Bonjour, combien êtes vous ?");
     scanf("%d", &guestCount);
-    int tableNumber = assignTable(guestCount);
-    if(tableNumber == 0) printf("Désoler vous êtes trop nombreux, je doit vous refuser.");
-    else printf("Très bien veuiller prendre la table %d", tableNumber);
+    int assignedTable = assignTable(guestCount);
+    if(assignedTable == 0) printf("Désoler vous êtes trop nombreux, je doit vous refuser.");
+    else printf("Très bien veuiller prendre la table %d", assignedTable);
+
+    printf("Que voulez vous en plats ?('c' pour couscous(15$), 't' pour tomates(8$) et 'r' pour risotto(23$))");
+    scanf("%c", &dish);
+    printf("Que voulez vous en boissons ?('s' pour soda(5$), 'e' pour eau(2$) et 'l' pour limonade(12$))");
+    scanf("%c", &drink);
+    printf("Que voulez vous en desserts ?('t' pour tiramisu(62$), 'g' pour glace(3$) et 'm' pour macaron(8$))");
+    scanf("%c", &dessert);
+
 
     return 0;
 }
